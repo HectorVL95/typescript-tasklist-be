db528a576e4c44d23113459ad1d93df3d5413468
Hector Vl <francisco1495@gmail.com>
2024-10-27 19:40:42 -0700

    created task model and started task create controller

---
+++ api/tasks/task-controllers.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from "express";
+import taskModel from "./task-model.js";
+
+const create_task = async (req: Request, res: Response, next: NextFunction) => {
+  
+}
--- api/tasks/task-model.js
+++ api/tasks/task-model.js
@@ -1,1 +1,7 @@
-export {};
+import { Schema, model } from "mongoose";
+const Task = new Schema({
+    name: {
+        type: String,
+    }
+});
+export default model('Task', Task);
